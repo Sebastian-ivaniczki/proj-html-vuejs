a11800e5cc53b93a2571da09c95c35bdc2c3da1c
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-01-31 11:22:51 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+export default {
+  components: { AppHeader, AppMain, AppFooter}
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+
+</style>
