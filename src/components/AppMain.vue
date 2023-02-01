7b62378116f05a5e081ee00adf50acb6f73b3c1f
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-02-01 12:48:42 +0100

    add dinamic vehicle card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,16 @@
 <script>
+import { store } from './data/store';
 import Filter from './partials/Filter.vue';
 import Cards from './partials/Cards.vue';
 export default {
   name: 'AppMain',
-  components: { Filter, Cards }
+  components: { Filter, Cards },
+  data() {
+    return {
+      store,
+      
+    }
+  }
 }
 
 </script>
--- src/components/partials/Cards.vue
+++ src/components/partials/Cards.vue
@@ -1,14 +1,44 @@
 <script>
+import { store } from '../data/store';
 export default {
-  name: 'Cards'
-}
+  name: 'Cards',
+  data() {
+    return {
+     store
+    }
+  },
+  methods: {
+    currentImage: function(img) {
+        return new URL(`../../assets/img/${img}.jpg`, import.meta.url).href;
+      }
+  }
+
+  }
 
 </script>
 
 <template>
- 
+  <div class="d-flex flex-wrap">
+
+    <div v-for="vehicle in store.vehicles"  class="card p-2" style="width: 20rem;">
+     <img :src="currentImage(vehicle.image)" class="card-img-top" :alt="vehicle.brand">
+     <div class="card-body">
+       <p class="card-text">{{ vehicle.brand }} {{ vehicle.type }}</p>
+       <p>{{ vehicle.category }}</p>
+       <span><strong>$</strong> {{ vehicle.price }}</span> 
+       <span><i class="fa-solid fa-car"></i>{{  vehicle.brand }}</span> 
+       <span><i class="fa-solid fa-gas-pump"></i>{{ vehicle.fuel }}</span>
+      </div>
+    </div>
+     
+  </div>
 </template>
+     
+  
+  
 
 <style scoped lang="scss">
-
+span{
+  margin-right: 1rem;
+}
 </style>
